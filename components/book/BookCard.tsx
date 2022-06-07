133d1eefd07e62a609ccd0d84720cd8b6d53e7a2
Reonowo <reo12062@gmail.com>
2022-06-07 20:37:56 +0700

    Main Menu Cards

---
+++ components/book/BookCard.tsx
@@ -0,0 +1,180 @@
+import React from "react";
+
+import {
+	Card,
+	Image,
+	Text,
+	Group,
+	Badge,
+	Button,
+	UnstyledButton,
+	ActionIcon,
+	createStyles,
+	useMantineTheme,
+	ScrollArea,
+	Divider,
+	ThemeIcon,
+} from "@mantine/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { classNames } from "@/utils/classNames";
+import { faCheck, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+const useStyles = createStyles((theme) => ({
+	card: {
+		backgroundColor:
+			theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+	},
+
+	section: {
+		borderBottom: `1px solid ${
+			theme.colorScheme === "dark"
+				? theme.colors.dark[4]
+				: theme.colors.gray[3]
+		}`,
+		paddingLeft: theme.spacing.md,
+		paddingRight: theme.spacing.md,
+		paddingBottom: theme.spacing.md,
+	},
+
+	like: {
+		color: theme.colors.red[6],
+	},
+
+	button: {
+		"&:hover": {
+			backgroundColor:
+				theme.colorScheme === "dark"
+					? theme.colors.blue[7]
+					: theme.colors.gray[2],
+		},
+	},
+
+	label: {
+		textTransform: "uppercase",
+		fontSize: theme.fontSizes.xs,
+		fontWeight: 700,
+	},
+}));
+
+interface BadgeCardProps {
+	image: string;
+	title: string;
+	main_category: string;
+	description: string;
+}
+
+export default function BookCard({
+	image,
+	title,
+	description,
+	main_category,
+}: BadgeCardProps) {
+	const { classes } = useStyles();
+	// const theme = useMantineTheme();
+
+	// const features = badges.map((badge) => (
+	// 	<Badge
+	// 		color={theme.colorScheme === "dark" ? "dark" : "gray"}
+	// 		key={badge.label}
+	// 		leftSection={badge.emoji}
+	// 	>
+	// 		{badge.label}
+	// 	</Badge>
+	// ));
+
+	// Current has bug at a certain width where the hearticon will go down a bit.
+	// idk how I'd fix that unless I hook onto the card's size,
+	// then turn it into a Menu xdddd
+	return (
+		<Card
+			withBorder
+			radius="md"
+			p="md"
+			className={classNames(classes.card, "min-h-full w-full max-w-md")}
+		>
+			<Card.Section>
+				<Image src={image} alt={title} height={180} />
+			</Card.Section>
+
+			<Card.Section className={classes.section} mt="md">
+				<Group position="apart">
+					<Group
+						direction="row"
+						spacing={0}
+						align="center"
+						className="w-full"
+					>
+						<div className="flex w-full items-center">
+							<div className="flex-grow truncate">
+								<Text
+									title={title}
+									size="lg"
+									weight={500}
+									className="truncate"
+								>
+									{title}
+								</Text>
+							</div>
+							<Badge size="sm" className="min-w-min">
+								{main_category}
+							</Badge>
+						</div>
+						{/* <Text size="xs" color={"dimmed"} className="ml-2">
+							Available
+						</Text> */}
+					</Group>
+				</Group>
+				<ScrollArea className="h-48 max-h-full">
+					<Text size="sm" mt="xs" className="min-h-full">
+						{title}: {description}
+					</Text>
+				</ScrollArea>
+			</Card.Section>
+
+			{/* <Card.Section className={classes.section}>
+				<Text mt="md" className={classes.label} color="dimmed">
+					Perfect for you, if you enjoy
+				</Text>
+				<Group spacing={7} mt={5}>
+					{features}
+				</Group>
+			</Card.Section> */}
+
+			<Group mt="xs">
+				<div className="flex flex-row items-center justify-center">
+					<UnstyledButton
+						className={classNames(
+							classes.button,
+							"rounded-md px-4 py-2"
+						)}
+					>
+						Details
+					</UnstyledButton>
+					<Divider className="m-2 h-8" orientation="vertical" />
+					<UnstyledButton
+						className={classNames(
+							classes.button,
+							"rounded-md px-4 py-2"
+						)}
+					>
+						Borrow
+					</UnstyledButton>
+				</div>
+
+				<ActionIcon
+					variant="default"
+					radius="md"
+					size={36}
+					className="ml-auto"
+				>
+					<FontAwesomeIcon
+						icon={faHeart}
+						size={"1x"}
+						className={classes.like}
+					/>
+				</ActionIcon>
+			</Group>
+		</Card>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,110 @@
 import React from "react";
 import { NextPage } from "next";
 
-import { ScrollArea } from "@mantine/core";
+import { Grid, ScrollArea } from "@mantine/core";
+import BookCard from "@/components/book/BookCard";
 
 const Home: NextPage = () => {
 	return (
-		<ScrollArea className="relative h-full max-h-1 min-h-full">
-			<div className="h-screen">This overflows!</div>
-		</ScrollArea>
+		<Grid justify="start" align="center">
+			<Grid.Col md={6} lg={3}>
+				<BookCard
+					title="A Hitchhiker's Guide to the Galaxy"
+					main_category="Adventure"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi!"
+					image={
+						"https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+					}
+				/>
+			</Grid.Col>
+			<Grid.Col md={6} lg={3}>
+				<BookCard
+					title="A Hitchhiker's Guide to the Galaxy"
+					main_category="Adventure"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi!"
+					image={
+						"https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+					}
+				/>
+			</Grid.Col>
+
+			<Grid.Col md={6} lg={3}>
+				<BookCard
+					title="Test Book"
+					main_category="Adventure"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi!"
+					image={
+						"https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+					}
+				/>
+			</Grid.Col>
+			<Grid.Col md={6} lg={3}>
+				<BookCard
+					title="Test Book"
+					main_category="Adventure"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi!"
+					image={
+						"https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+					}
+				/>
+			</Grid.Col>
+
+			<Grid.Col md={6} lg={3}>
+				<BookCard
+					title="Test Book"
+					main_category="Adventure"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi!"
+					image={
+						"https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+					}
+				/>
+			</Grid.Col>
+			<Grid.Col md={6} lg={3}>
+				<BookCard
+					title="A Hitchhiker's Guide to the Galaxy"
+					main_category="Adventure"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi!"
+					image={
+						"https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+					}
+				/>
+			</Grid.Col>
+
+			<Grid.Col md={6} lg={3}>
+				<BookCard
+					title="Test Book"
+					main_category="Adventure"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi!"
+					image={
+						"https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+					}
+				/>
+			</Grid.Col>
+			<Grid.Col md={6} lg={3}>
+				<BookCard
+					title="Test Book"
+					main_category="Adventure"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi!"
+					image={
+						"https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+					}
+				/>
+			</Grid.Col>
+
+			<Grid.Col md={6} lg={3}>
+				<BookCard
+					title="Test Book"
+					main_category="Adventure"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt perspiciatis voluptatum beatae dolorem deleniti harum sapiente minima voluptas modi!"
+					image={
+						"https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+					}
+				/>
+			</Grid.Col>
+		</Grid>
+		// <ScrollArea className="relative h-full max-h-1 min-h-full w-full max-w-full overflow-x-hidden">
+
+		// </ScrollArea>
 	);
 };
 
